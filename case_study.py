58e18c236d08ab486afb2612ea50d0c4ad7666b0
Mohammad Meraj <meet.meraj2000@gmail.com>
2024-05-05 21:09:02 +0530

    second_change

--- case_study.py
+++ case_study.py
@@ -1,15 +1,22 @@
 from openai import OpenAI
 import os
+import json
 
 x = os.listdir('/Users/mohammadmeraj/Desktop/case_study_koodoo/casestudy')
 y = [i for i in x if ".mp3" in i ]
+f = open("key.json")
+data = json.load(f)
 
 for k in y:
-  client = OpenAI(api_key = "sk-proj-eWpJcqP0sr7IWh9aUi0UT3BlbkFJeE6rpFjZUYj1qRD3dbcR")
+  client = OpenAI(api_key = data["key"])
   audio_file= open(k, "rb")
   transcription = client.audio.transcriptions.create(model="whisper-1", response_format="text",file=audio_file)
   print(transcription.text)
   print("--------------")
   print(audio_file)
+
+
+
+
 
 
